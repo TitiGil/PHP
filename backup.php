f95f78294b35bbb3ce130883670b4b5512054529
TitiGil <34870117+TitiGil@users.noreply.github.com>
2025-01-28 11:27:15 +0330

    Update backup.php
    remove directory added

--- backup.php
+++ backup.php
@@ -1,6 +1,32 @@
-$files = glob('path/to/temp/*'); // get all file names
+"<?php
+
+function removeDir(string $dir): void {
+    $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+    $files = new RecursiveIteratorIterator($it,
+                 RecursiveIteratorIterator::CHILD_FIRST);
+    foreach($files as $file) {
+        if ($file->isDir()){
+            rmdir($file->getPathname());
+        } else {
+            unlink($file->getPathname());
+        }
+    }
+    rmdir($dir);
+}
+
+
+
+$files = glob(dirname(__FILE__).'/*'); // get all file names
+
 foreach($files as $file){ // iterate files
+echo($file);
   if(is_file($file)) {
     unlink($file); // delete file
+    echo('\n >> deleted!'."\n");
+  }
+  if(is_dir($file)){
+   removeDir($file);
+     echo(' >> deleted!'."\n");
   }
 }
+?>
